ffe55e0ba8dca337ed8ace9937ffbae443aab188
AlbertDavletov <albert.m.davletov@gmail.com>
2023-08-27 16:57:49 -0700

    Add Bitmap game

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ public class Program
 	public static void Main()
     {
         Console.WriteLine("U Games");
-        GuessNumberGame();
+        BitMapGame();
     }
 
     static void GuessNumberGame()
     {
         var guessNumber = new GuessNumber();
         guessNumber.Play();
+    }
+
+    static void BitMapGame()
+    {
+        var bitmap = new Bitmap();
+        bitmap.Play();
     }
 }
---
+++ games/Bitmap.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unilecs.Games
+{
+    internal class Bitmap
+    {
+        private const string Map =
+@"
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+....................................................................
+";
+
+        public const string Name = "BitMap";
+        public const string Description = "BitMap Game";
+
+        public void Play()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Description);
+
+            while (true)
+            {
+                Console.WriteLine("\nWanna play?! Let's play !");
+                Console.WriteLine("Enter the message to display with the bitmap.");
+
+                string message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message))
+                {
+                    for (int i = 0; i < Map.Length; i++)
+                    {
+                        if (Map[i] != '*')
+                        {
+                            Console.Write(Map[i]);
+                        }
+                        else
+                        {
+                            Console.Write(message[i % message.Length]);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Wanna play again?!");
+                var answer = Console.ReadLine();
+                if (answer == null || !answer.Contains("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Thanks for this game!");
+        }
+    }
+}
